bb92d7374f3b3897bce6fc2e1c3984ca3ca95600
PaperPlane01 <dagfortnewage@gmail.com>
2017-06-30 05:27:00 +0600

    App updated

--- src/main/java/kz/javalab/texthandling/text/entity/impl/sentencepart/SentencePart.java
+++ src/main/java/kz/javalab/texthandling/text/entity/impl/sentencepart/SentencePart.java
@@ -2,10 +2,6 @@ package kz.javalab.texthandling.text.entity.impl.sentencepart;
 
 import kz.javalab.texthandling.text.entity.Text;
 
-
-/**
- *
- */
 public abstract class SentencePart implements Text {
 
     @Override
--- src/main/java/kz/javalab/texthandling/text/view/impl/TextView.java
+++ src/main/java/kz/javalab/texthandling/text/view/impl/TextView.java
@@ -4,6 +4,9 @@ package kz.javalab.texthandling.text.view.impl;
 import kz.javalab.texthandling.text.entity.Text;
 import kz.javalab.texthandling.text.view.View;
 
+/**
+ * This class is designated to visually represent content of <Code>Text</Code> instance.
+ */
 public class TextView implements View {
 
     private Text model;
@@ -24,6 +27,9 @@ public class TextView implements View {
         this.model = model;
     }
 
+    /**
+     * Shows data contained in <Code>Text</Code> instance.
+     */
     @Override
     public void show() {
         System.out.println(model.getContent());
